d593d1dd6725e629c3ece34e7b5b7ac2770a08c8
Exabiier <exabiier.bars@gmail.com>
2022-11-03 16:53:22 -0500

    Made Albums

--- albums_migrations.sql
+++ albums_migrations.sql
@@ -1,1 +1,11 @@
 USE codeup_test_db;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist_name VARCHAR(100) NOT NULL,
+                        record_name  VARCHAR(100) NOT NULL,
+                        release_date INT(4) UNSIGNED NOT NULL,
+                        sales FLOAT NOT NULL,
+                        genre_of_record TEXT NOT NULL,
+                        PRIMARY KEY (id)
+);

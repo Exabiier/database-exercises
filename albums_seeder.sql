59fa89b53eb38ec454278b0d845abaaa69b2125b
Exabiier <exabiier.bars@gmail.com>
2022-11-06 22:11:59 -0600

    finished exircise 1

---
+++ albums_seeder.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+    SELECT * FROM albums;
+    SELECT * FROM albums WHERE release_data < 1980;
+    SELECT * FROM albums WHERE artist_name = 'Michael Jackson';
+
+UPDATE albums
+SET sales * 10;
+
+UPDATE albums
+SET release_date - 100
+WHERE release_date < 1980;
+
+U
+
---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+
+USE codeup_test_db;
+SELECT * FROM albums WHERE artist_name = 'Pink Floyd';
+
+USE codeup_test_db;
+SELECT * FROM albums WHERE record_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT * FROM albums WHERE release_date = 1990;
+
+SELECT * FROM albums WHERE sales <= 20;
+
+SELECT * FROM albums WHERE genre_of_record = 'Rock';
+
